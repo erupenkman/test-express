f17bc5dfb0720c202c58481065758841ebb00533
Eru Penkman <eru@Erus-MacBook-Pro.local>
2015-01-18 18:36:56 +1000

    first post

---
+++ server.js
@@ -0,0 +1,30 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http');
+
+var app = express();
+var server = app.listen(3000);
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+	res.send(200,"Hello World");
+});
+
+console.log("Express server listening on port ", 3000);
